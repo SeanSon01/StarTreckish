11128dc9eb3ee746a8a85b0ec029cb228c29548b
Nates Mother <slpar813@learner.dvusd.org>
2017-02-21 11:23:14 -0700

    3

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -123,16 +123,6 @@ namespace BlasterMaster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap download__2_ {
-            get {
-                object obj = ResourceManager.GetObject("download__2_", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap enemyA1 {
             get {
                 object obj = ResourceManager.GetObject("enemyA1", resourceCulture);
